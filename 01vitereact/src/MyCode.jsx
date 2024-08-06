d55817765c2c4bc8ddb43ce7d7747ce8d3f3c088
2812SAM <124618164+2812SAM@users.noreply.github.com>
2024-08-06 15:03:21 +0000

    .

---
+++ 01vitereact/src/MyCode.jsx
@@ -0,0 +1,8 @@
+function myCode(){
+    return (<div>
+        <h1>Hey I'm Learning React Library</h1>
+        <input type="checkbox" />
+    </div>
+    )
+}
+export default myCode;
---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import MyCode from './MyCode.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div>
+    <App />
+    <MyCode />
+      </div>
+  </React.StrictMode>
+)
