dd47e1151ddde9d54a0d18790ddb99cc27bb6e32
Swayam Sharma <124618164+2812SAM@users.noreply.github.com>
2024-12-29 17:17:40 +0000

    mm

--- 03Tailwind/03Tailwind/src/App.jsx
+++ 03Tailwind/03Tailwind/src/App.jsx
@@ -6,12 +6,20 @@ import Card from './Components/card'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  let myObj={
+    name:"Regret",
+    age:21
+  }
+  let myObj2={
+    name:"Sucess",
+    age:24
+  }
+  let Array= [1,2,3]
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind</h1>
-      <Card />
-      <Card />
+      <Card username = "Swayam" obj={myObj}/>
+      <Card username = "no-one" obj={myObj2}/>
     </>
   )
 }
--- 03Tailwind/03Tailwind/src/Components/card.jsx
+++ 03Tailwind/03Tailwind/src/Components/card.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-function Card(){
+function Card(props){
     return(
         <div className="max-w-xs p-6 rounded-md shadow-md bg-black">
         <img
@@ -8,14 +8,13 @@ function Card(){
           className="object-cover object-center w-full rounded-md h-72 bg-gray-500"
         />
         <div className="mt-6 mb-2">
-          <span className="block text-sm font-medium font-mono tracking-widest uppercase text-indigo-400">
-            Title
+          <span className="block text-sm font-medium font-mono tracking-widest uppercase text-indigo-400">Title
           </span>
-          <h2 className="text-xl font-semibold tracking-wide">Lorem ipsum dolor</h2>
+          <h2 className="text-xl font-semibold tracking-wide">{props.username}</h2>
         </div>
         <p className="text-gray-300">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio tempora ipsum soluta
-          amet
+          amet{props.obj.age}
         </p>
       </div>
     )
