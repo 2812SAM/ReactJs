070c82d542139e55a1aec38c86c75b36c7a5d941
2812SAM <swayam.kaushik@gmail.com>
2025-01-09 14:57:53 +0530

    m

---
+++ miniContext/src/context/UserContextProvider.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import UserContext from "./UserContextserContext";
+
+const UserContextserContext = ({children}) => {
+    return (
+        <>
+        {children}
+
+        </>
+    )
+}
